37db5c45f4df9471f488da5fd330929725594f01
Daniel Wertheim <daniel@wertheim.se>
2015-05-10 00:18:19 +0200

    Fixed lic, bumped v0.1.1

--- deploy/gulpfile.js
+++ deploy/gulpfile.js
@@ -13,7 +13,7 @@ var config = {
   src: './../src/',
   build: {
     outdir: './build/',
-    version: '0.1.0',
+    version: '0.1.1',
     revision: argv.buildrevision || '0',
     profile: argv.buildprofile || 'Release'
   }
