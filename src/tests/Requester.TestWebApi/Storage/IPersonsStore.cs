62c40831cb9b059db62fd399adb86763255da9bf
Daniel Wertheim <daniel@wertheim.se>
2015-10-20 22:23:30 +0200

    Ninject for tests just to see...

---
+++ src/tests/Requester.TestWebApi/Storage/IPersonsStore.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Requester.TestWebApi.Model;
+
+namespace Requester.TestWebApi.Storage
+{
+    public interface IPersonsStore
+    {
+        void Store(Person person);
+        Person Get(Guid id);
+    }
+}
---
+++ src/tests/Requester.TestWebApi/Storage/InMemPersonStore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Concurrent;
+using Requester.TestWebApi.Model;
+
+namespace Requester.TestWebApi.Storage
+{
+    public class InMemPersonStore : IPersonsStore
+    {
+        private readonly ConcurrentDictionary<Guid, Person> _personState = new ConcurrentDictionary<Guid, Person>();
+
+        public void Store(Person person)
+        {
+            _personState.AddOrUpdate(person.Id, person, (eid, ep) => person);
+        }
+
+        public Person Get(Guid id)
+        {
+            Person r;
+            return _personState.TryGetValue(id, out r) ? r : null;
+        }
+    }
+}
