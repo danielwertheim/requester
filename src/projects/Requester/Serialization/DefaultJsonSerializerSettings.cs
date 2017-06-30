5cd7886efd942933c4c03f7a68b5ffd986a14980
Daniel Wertheim <daniel@wertheim.se>
2017-06-30 07:16:42 +0200

    Closes #17, Serialization defaults
    Be explicit and mimic ASP.NET Core
    
      - Do not process dictionary keys
      - Do not override specified names
      - Do not process extension data names
      - Exclude type names
      - Missing member should not blow

--- src/projects/Requester/Serialization/DefaultJsonSerializerSettings.cs
+++ src/projects/Requester/Serialization/DefaultJsonSerializerSettings.cs
@@ -1,3 +1,4 @@
+using System;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
@@ -6,23 +7,28 @@ namespace Requester.Serialization
 {
     public static class DefaultJsonSerializerSettings
     {
-        public static IContractResolver DefaultContractResolver = new CamelCasePropertyNamesContractResolver();
-
-        public static JsonSerializerSettings Create()
+        public static IContractResolver DefaultContractResolver = new DefaultContractResolver
         {
-            return ApplyTo(new JsonSerializerSettings());
-        }
+            NamingStrategy = new CamelCaseNamingStrategy(false, false, false)
+        };
 
-        public static JsonSerializerSettings ApplyTo(JsonSerializerSettings settings)
-        {
-            settings.NullValueHandling = NullValueHandling.Ignore;
-            settings.DateFormatHandling = DateFormatHandling.IsoDateFormat;
-            settings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
+        public static Func<JsonSerializerSettings, JsonSerializerSettings> Applier { private get; set; } =
+            settings =>
+            {
+                settings.NullValueHandling = NullValueHandling.Ignore;
+                settings.DateFormatHandling = DateFormatHandling.IsoDateFormat;
+                settings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
+                settings.MissingMemberHandling = MissingMemberHandling.Ignore;
+                settings.TypeNameHandling = TypeNameHandling.None;
 
-            settings.ContractResolver = DefaultContractResolver;
-            settings.Converters.Add(new StringEnumConverter());
+                settings.ContractResolver = DefaultContractResolver;
+                settings.Converters.Add(new StringEnumConverter());
 
-            return settings;
-        }
+                return settings;
+            };
+
+        public static JsonSerializerSettings Create() => ApplyTo(new JsonSerializerSettings());
+
+        public static JsonSerializerSettings ApplyTo(JsonSerializerSettings settings) => Applier(settings);
     }
 }
---
+++ src/tests/Requester.UnitTests/Serialization/DefaultJsonSerializerTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Requester.Serialization;
+using Xunit;
+
+namespace Requester.UnitTests.Serialization
+{
+    public class DefaultJsonSerializerTests : UnitTestsOf<DefaultJsonSerializer>
+    {
+        public DefaultJsonSerializerTests()
+        {
+            UnitUnderTest = new DefaultJsonSerializer();
+        }
+
+        [Fact]
+        public void Should_not_touch_dictionary_keys()
+        {
+            var orgData = new SomeData
+            {
+                SomeKeys = new Dictionary<string, int>
+                {
+                    { "TestString1", 1 },
+                    { "TESTSTRING2", 2 },
+                    { "teststring3", 3 },
+                }
+            };
+
+            var json = UnitUnderTest.Serialize(orgData);
+
+            var reconstructed = UnitUnderTest.Deserialize<SomeData>(json);
+
+            foreach (var kv in orgData.SomeKeys)
+                reconstructed.SomeKeys.ContainsKey(kv.Key).Should().BeTrue();
+        }
+
+        private class SomeData
+        {
+            public Dictionary<string, int> SomeKeys { get; set; }
+        }
+    }
+}
